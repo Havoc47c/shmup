235a4b8195c5ff9e029d85623d036a4be44e0a17
Carey Gunter <carey.gunter7@gmail.com>
2018-12-28 15:10:16 -0800

    For better or worse I decided to add chrono literals to the interface of Tick.h. Only time will tell if this is a bad decision... or better way to do it.

--- src/Tick.h
+++ src/Tick.h
@@ -2,8 +2,10 @@
 
 #include <chrono>
 
+using namespace std::chrono_literals;
+
 namespace tick {
-using Duration = std::chrono::duration<float>;
+using Duration = std::chrono::duration<double>;
 using ExactDuration = std::chrono::duration<long long, std::nano>;
 using Instant = std::chrono::time_point<std::chrono::steady_clock, ExactDuration>;
 
